ebd83d629e7b1f175b2d0ac13e2cd8eb46305e95
sari7happy <sari7happy@gmail.com>
2022-10-22 16:24:20 +0900

    　ヒアドキュメント

--- const.php
+++ const.php
@@ -1,7 +1,4 @@
 <!-- const定数 -->
 <?php
-const TAX ='1.1';
-$price =1000;
-$sum =$price*TAX;
-echo $sum;
+echo 'rrr';
 ?>
---
+++ heredoc.php
@@ -0,0 +1,10 @@
+<?php
+$str ='PHP(PHP:Hypertext Preprocessor)';
+$msg =<<<EOD
+{$str}は、サーバーサイドで動作する簡易的なスクリプト言語です。
+まずは基礎を固めましょう！
+</br>
+"let's start,everyone!"
+EOD;
+echo $msg;
+?>
