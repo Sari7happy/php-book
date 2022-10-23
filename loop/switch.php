13e4b5636ec48730df181f41b9294dfd2b3851bd
kentarokinoshota@KentaronoMacBook-Pro <sari7happy@gmail.com>
2022-10-23 13:39:47 +0900

    switch多岐分岐

---
+++ loop/switch.php
@@ -0,0 +1,13 @@
+<?php
+$rank ='乙';
+if($rank =='甲'){
+    print '大変良いです';
+}elseif ($rank =='乙'){
+    print '良いです';
+}elseif($rank =='丙'){
+    print 'もう少し頑張りましょう';
+}else{
+    print '???';
+}
+
+?>
