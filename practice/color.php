b877b7afddaacaf15f980a25a96f3c27c9c7c3ad
kentarokinoshota@KentaronoMacBook-Pro <sari7happy@gmail.com>
2022-10-24 21:09:33 +0900

    ベーシックなプログラム

--- practice/color.php
+++ practice/color.php
@@ -1,15 +1,18 @@
-<?php $colors =['赤','青','黄','緑','オレンジ'];?>
+<?php $colors = ['赤', '青', '黄', '緑', 'オレンジ']; ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
     <title>ラッキーカラー占い</title>
 </head>
+
 <body>
-    <h1>本日、<?=data('Y年m月d日')?>のラッキーカラー</h1>
-    <p>今日のラッキーカラーは「<?=$colors[random_int(0,4)]?>」です！</p>
-    
+    <h1>本日、<?= date('Y年m月d日') ?>のラッキーカラー</h1>
+    <p>今日のラッキーカラーは「<?= $colors[random_int(0, 4)] ?>」です！</p>
+
 </body>
+
 </html>
---
+++ practice/hello.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>PHPでHello World!</h1>
+    <?php $greeting='こんにちは世界！'; ?>
+    <p><?php echo $greeting;?></p>
+</body>
+</html>
