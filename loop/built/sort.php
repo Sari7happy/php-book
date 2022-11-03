2f8bd6adff08b4e557b5f40e04857f743f9d5a7c
kentarokinoshota@KentaronoMacBook-Pro <sari7happy@gmail.com>
2022-11-03 22:47:38 +0900

    ソート関数値/key

--- loop/built/sort.php
+++ loop/built/sort.php
@@ -11,4 +11,8 @@ $data2=['Tennis'=>1,'Swimming'=>1,'Soccer'=>11,'Baseball'=>9];
 sort($data2,SORT_NUMERIC);
 print_r($data2);
 //数値で表すソート
+$data3=['Tennis'=>1,'Swimming'=>1,'Soccer'=>11,'Baseball'=>9];
+asort($data3,SORT_NUMERIC);
+print_r($data3);
+
 ?>
