8e8f5004671d71e67d10da778ba66ecd61c6a179
kentarokinoshota@KentaronoMacBook-Pro <sari7happy@gmail.com>
2022-11-03 22:44:29 +0900

    数値で表すソート関数

--- loop/built/sort.php
+++ loop/built/sort.php
@@ -7,5 +7,8 @@ print_r($data);
 rsort($data,SORT_STRING);
 print_r($data);
 //Z-Aに並べる
-
+$data2=['Tennis'=>1,'Swimming'=>1,'Soccer'=>11,'Baseball'=>9];
+sort($data2,SORT_NUMERIC);
+print_r($data2);
+//数値で表すソート
 ?>
