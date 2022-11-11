2de9eeeae93765d1f524d6aec1d7e93ec724c108
kentarokinoshota@KentaronoMacBook-Pro <sari7happy@gmail.com>
2022-11-11 19:49:46 +0900

    e関数

---
+++ practice/POST/check.php
@@ -0,0 +1,6 @@
+<input id="php" type="checkbox" name="lang[]" value="PHP" />
+<label for="php">PHP</label>
+<input id="Java" type="checkbox" name="lang[]" value="Java" />
+<label for="Java">Java</label>
+<input id="csharp" type="checkbox" name="lang[]" value="C#" />
+<label for="csharp">C#</label>
